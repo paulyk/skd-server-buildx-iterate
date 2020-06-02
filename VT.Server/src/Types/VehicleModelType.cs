4eb8bc18829f34f72820f28a5e253d49c731d27d
Paul Krisadaolarn <pauly.k@gmail.com>
2020-06-02 11:24:55 +0700

    VehicleModelType resolvers now async

--- VT.Server/src/Types/VehicleModelType.cs
+++ VT.Server/src/Types/VehicleModelType.cs
@@ -2,12 +2,14 @@
 
 using HotChocolate;
 using HotChocolate.Types;
+using HotChocolate.Resolvers;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using VT.Model;
 using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
 
 namespace VT.Server {
 
@@ -18,33 +20,24 @@ namespace VT.Server {
             descriptor.Field(t => t.Id).Type<NonNullType<IdType>>();
             // descriptor.Field(t => t.ComponentMappings).Ignore();
             descriptor.Field(t => t.ActiveComponentMappings).Ignore();
-       
-            descriptor.Field(t => t.ComponentMappings)
-                .Resolver(ctx => ctx.Service<AppDbContext>().VehicleModelComponents
+            descriptor.Field(t => t.ComponentMappings).Resolver(GetVheicleModel_Components);
+            descriptor.Field(t => t.Vehicles).Resolver(GetVehicleModel_Vehicles);
+        }
+
+        public async Task<List<VehicleModelComponent>> GetVheicleModel_Components(IResolverContext ctx) {
+            Console.WriteLine("GetVheicleModel_Components");
+            return await ctx.Service<AppDbContext>().VehicleModelComponents
                      .AsNoTracking()
                      .Include(t => t.Component)
-                     .Where(t => t.VehicleModelId == ctx.Parent<VehicleModel>().Id)                     
-                     .ToList()
-                );                
-
-            descriptor.Field(t => t.Vehicles)
-                .Resolver(ctx => ctx.Service<AppDbContext>().Vehicles
-                    .Where(t => t.ModelId == ctx.Parent<VehicleModel>().Id).ToList()
-                );
+                     .Where(t => t.VehicleModelId == ctx.Parent<VehicleModel>().Id)
+                     .ToListAsync();
         }
-    }
 
-    public class ComponentSequence {
-        public Guid Id { get; set; }
-
-        public string Code { get; set; } = "";
-
-        public string Name { get; set; } = "";
-
-        public int Sequence { get; set; } = 0;
-
-        public DateTime CreatedAt { get; set; }
-        public DateTime? RemovedAt { get; set; }
+        public async Task<List<Vehicle>> GetVehicleModel_Vehicles(IResolverContext ctx) {
+            Console.WriteLine("GetVehicleModel_Vehicles");
+            return await ctx.Service<AppDbContext>().Vehicles
+                    .Where(t => t.ModelId == ctx.Parent<VehicleModel>().Id).ToListAsync();
+        }
     }
 
 }
