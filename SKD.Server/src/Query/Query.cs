55a52e852cebcb6f260fd42a55a65e9eb43c3a9e
Paul Krisadaolarn <pauly.k@gmail.com>
2020-06-30 12:12:38 +0700

    paging

--- SKD.Server/src/Query/Query.cs
+++ SKD.Server/src/Query/Query.cs
@@ -1,42 +1,26 @@
-#nullable enable
-
 using System;
-using HotChocolate.Types;
-using Microsoft.Extensions.Logging;
-using Microsoft.EntityFrameworkCore;
 using System.Linq;
-using SKD.Model;
-using HotChocolate;
-using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
 using System.Threading.Tasks;
+using System.Collections.Generic;
+using HotChocolate;
+using SKD.Model;
+
 
 namespace SKD.Server {
 
     public class Query {
 
-        public string Hello() => "Welcome to the RMA Vehicle Component Tracking Graphql Server";
-
-        public async Task<IReadOnlyList<Component>> GetComponents([Service] AppDbContext ctx, int first = Int32.MaxValue) {
-            return await ctx.Components
-                .AsNoTracking().OrderBy(t => t.Code).Take(first).ToListAsync();
-        }
+        public string Info() => "RMA vehicle component scanning service";
 
         public async Task<Component> GetComponentByCode([Service] AppDbContext ctx, string code) {
             return await ctx.Components.AsNoTracking().FirstOrDefaultAsync(t => t.Code == code);
         }
 
-        public async Task<IReadOnlyList<VehicleModel>> GetVehicleModels([Service] AppDbContext ctx, int first = Int32.MaxValue) {
-            return await ctx.VehicleModels
-                .AsNoTracking().OrderBy(t => t.Code).Take(first).ToListAsync();
-        }
-
         public async Task<VehicleModel> GetVehicleModelByCode([Service] AppDbContext ctx, string code) {
             return await ctx.VehicleModels.AsNoTracking().FirstOrDefaultAsync(t => t.Code == code);
         }
-        public async Task<IReadOnlyList<Vehicle>> GetVehicles([Service] AppDbContext ctx, int first = Int32.MaxValue) {
-            return await ctx.Vehicles
-                .AsNoTracking().OrderBy(t => t.Model.Code).Take(first).ToListAsync();
-        }
+
         public async Task<Vehicle> GetVehicleByVIN([Service] AppDbContext ctx, string vin) {
             return await ctx.Vehicles.AsNoTracking().FirstOrDefaultAsync(t => t.VIN == vin);
         }
--- SKD.Server/src/Types/QueryType.cs
+++ SKD.Server/src/Types/QueryType.cs
@@ -1,18 +1,20 @@
-# nullable enable
-#nullable enable
-
-using HotChocolate;
-using HotChocolate.Resolvers;
-using HotChocolate.Types;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using SKD.Model;
-
+using HotChocolate.Types;
+using HotChocolate.Types.Relay;
 namespace SKD.Server {
     public class QueryType: ObjectType<Query> {
         protected override void Configure(IObjectTypeDescriptor<Query> descriptor) {
+              descriptor.Field("getComponents")
+                .Resolver(ctx => ctx.Service<AppDbContext>().Components.AsQueryable())
+                .UsePaging<ComponentType>();
+
+            descriptor.Field("getVehicleModles")
+                .Resolver(ctx => ctx.Service<AppDbContext>().VehicleModels.AsQueryable())
+                .UsePaging<VehicleModelType>();
+
+            descriptor.Field("getVehicles")
+                .Resolver(ctx => ctx.Service<AppDbContext>().Vehicles.AsQueryable())
+                .UsePaging<VehicleType>();
         }
     }
 }
